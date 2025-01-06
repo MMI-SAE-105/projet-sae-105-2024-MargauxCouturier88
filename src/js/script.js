233c690d007bdbe04ee209ae3765539dd37e232a
MargauxCouturier88 <margaux.couturier02@edu.univ-fcomte.fr>
2025-01-06 10:56:12 +0100

    menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,5 @@
+//Menu
+
 const toggle = document.querySelector(".headermenu-btn");
 const nav = document.querySelector("#mainNav");
 const page = document.body;
@@ -7,11 +9,9 @@ if (toggle && nav) {
         const isOpen = toggle.getAttribute("aria-expanded") === "true";
         const isClosed = !isOpen;
 
-        // Met à jour les attributs ARIA pour l'accessibilité
         toggle.setAttribute("aria-expanded", isClosed);
         nav.setAttribute("aria-hidden", isOpen);
 
-        // Ajoute ou supprime la classe noscroll sur le body
         page.classList.toggle("noscroll", isClosed);
     });
 }
