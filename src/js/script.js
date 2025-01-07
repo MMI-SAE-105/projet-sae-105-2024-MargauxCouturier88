7f472ed7984bc7b807190b330f28a53fce907175
MargauxCouturier88 <margaux.couturier02@edu.univ-fcomte.fr>
2025-01-07 16:34:39 +0100

    Carrousel css js

--- src/js/script.js
+++ src/js/script.js
@@ -15,3 +15,35 @@ if (toggle && nav) {
         page.classList.toggle("noscroll", isClosed);
     });
 }
+
+//Carrousel
+
+const prevButton = document.querySelector('.carousel-button.prev');
+const nextButton = document.querySelector('.carousel-button.next');
+const carousel = document.querySelector('.card__projet');
+
+let scrollAmount = 0;
+const cardWidth = 270; 
+
+prevButton.addEventListener('click', () => {
+ 
+    if (scrollAmount > 0) {
+        scrollAmount -= cardWidth;
+        carousel.scrollTo({
+            left: scrollAmount,
+            behavior: 'smooth' 
+        });
+    }
+});
+
+nextButton.addEventListener('click', () => {
+
+    if (scrollAmount < carousel.scrollWidth - carousel.clientWidth) {
+        scrollAmount += cardWidth;
+        carousel.scrollTo({
+            left: scrollAmount,
+            behavior: 'smooth'
+        });
+    }
+});
+
